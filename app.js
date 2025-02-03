97e9f4d6516b6bf96147963e56e507162aa0d7ef
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-02-03 14:23:39 +0100

    🚧  A poc version exposing all UI

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require("express");
-const app = express();
+// https://www.npmjs.com/package/ejs
+const ejs = require("ejs");
+const fs = require("fs");
 const axios = require("axios");
 
-// Affiche les données en JSON pour la requête GET '/' par défaut.
+const app = express();
+
+const indexPath = __dirname + "/views/index.ejs";
+console.log("getting index template at " + indexPath);
+
+let index = ejs.compile(fs.readFileSync(indexPath, "utf8"));
+
 app.get("/", (req, res) => {
-  const year = req.query.year; // Récupère le paramètre 'year' de la query string
-  if (!year) {
-    return res.status(400).send({ error: "Veuillez spécifier l'année" });
-  }
-
-  const urlAPI = `https://api.iucnredlist.org/api/v1/search?taxonName=&year=${year}&format=json&fields=name,conservationStatus`;
-
-  axios
-    .get(urlAPI)
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({
-        error: "Une erreur est survenue lors de la récupération des données",
-      });
-    });
+  res.send(
+    index({
+      pays: [{ name: "France", id: "FR" }],
+      species: [
+        {
+          name: "Requin-taupe commun",
+          type: "type ?",
+          status: "status ?",
+          year: "Année ?",
+        },
+      ],
+    }),
+  );
 });
-
 // Démarrage du serveur
 const port = 3000;
 app.listen(port, () => {
