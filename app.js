05c150c2a425fc2aa3c36d6fa94e4647ca2a1065
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-02-04 08:47:27 +0100

    🚧 Seems like it works ... on a very basic level ... We do not fetch yet all taxons, and we do not use local labels (and don't show the link)

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ const express = require("express");
 const ejs = require("ejs");
 const fs = require("fs");
 const axios = require("axios");
+const rateLimit = require("axios-rate-limit");
+require("dotenv").config();
 
 const app = express();
+
+const IUCN_AUTH_HEADER = {
+  headers: {
+    Authorization: process.env.IUCN_API_TOKEN,
+  },
+};
+const iucnClient = rateLimit(axios.create(), {
+  maxRequests: 1,
+  perMilliseconds: 1000,
+  maxRPS: 2,
+});
+
+function extractCountryInfos(country) {
+  return {
+    name: country.name.common,
+    id: country.cca2,
+  };
+}
+
+let countries = [];
+// Use axios to fetch country list from restcountries api
+console.log("Using axios to load countries list");
+iucnClient.get("https://restcountries.com/v3.1/all").then(function (response) {
+  let inputCountries = response.data.map(extractCountryInfos);
+  inputCountries.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  countries = countries.concat(inputCountries);
+  console.log(`Obtained ${countries.length} countries`);
+});
 
 const indexPath = __dirname + "/views/index.ejs";
 console.log("getting index template at " + indexPath);
@@ -14,11 +46,10 @@ let index = ejs.compile(fs.readFileSync(indexPath, "utf8"));
 app.get("/", (req, res) => {
   res.send(
     index({
-      pays: [{ name: "France", id: "FR" }],
+      pays: countries,
       species: [
         {
           name: "Requin-taupe commun",
-          type: "type ?",
           status: "status ?",
           year: "Année ?",
         },
@@ -26,6 +57,46 @@ app.get("/", (req, res) => {
     }),
   );
 });
+
+app.get("/get-species-for/:codePays", async function (req, res) {
+  console.info(`Searching for endangered species in "${req.params.codePays}"`);
+  // This is dangerous !
+  const url = `https://api.iucnredlist.org/api/v4/countries/${req.params.codePays}`;
+  return await iucnClient
+    .get(url, IUCN_AUTH_HEADER)
+    .then(function (response) {
+      let assessments = response.data.assessments;
+      assessments = assessments.slice(0, Math.min(assessments.length, 10));
+      console.info(`Processing ${assessments.length} endangered species`);
+      // Hack that to check some things
+      Promise.all(
+        assessments.map(function (assessment) {
+          const assessmentId = assessment.assessment_id;
+          const url = `https://api.iucnredlist.org/api/v4/assessment/${assessmentId}`;
+          return iucnClient.get(url, IUCN_AUTH_HEADER);
+        }),
+      ).then(function (responseArray) {
+        let transformedAssessments = responseArray.map(function (response) {
+          console.info(`Processing ${response.data.taxon.scientific_name}`);
+          return {
+            name: response.data.taxon.scientific_name,
+            type: "",
+            status: response.data.red_list_category.description.en,
+            year: response.data.year_published,
+          };
+        });
+        transformedAssessments.sort((a, b) => a.name.localeCompare(b.name));
+        res.send(transformedAssessments);
+      });
+    })
+    .catch(function (error) {
+      console.error(
+        `Fetching endangered species in ${req.params.codePays} returned ${error.message}`,
+      );
+      res.status(500).send(error.message);
+    });
+});
+
 // Démarrage du serveur
 const port = 3000;
 app.listen(port, () => {
