404d035713ba7025aa034e747a89f5ddd12f1d85
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2025-02-04 16:32:01 +0100

    📦️ Ensuring packaging is correct and compatible with async functions

--- src/app.js
+++ src/app.js
@@ -13,82 +13,89 @@ const IUCN_AUTH_HEADER = {
   },
 };
 
-let countries = await getCountries();
+async function main() {
+  let countries = await getCountries();
 
-const indexPath = __dirname + "/views/index.ejs";
-console.log("getting index template at " + indexPath);
+  const indexPath = __dirname + "/views/index.ejs";
+  console.log("getting index template at " + indexPath);
 
-let index = ejs.compile(fs.readFileSync(indexPath, "utf8"));
+  let index = ejs.compile(fs.readFileSync(indexPath, "utf8"));
 
-app.get("/", (req, res) => {
-  res.send(
-    index({
-      pays: countries,
-      species: [
-        {
-          name: "Requin-taupe commun",
-          status: "status ?",
-          year: "Année ?",
-        },
-      ],
-    }),
-  );
-});
+  app.get("/", (req, res) => {
+    res.send(
+      index({
+        pays: countries,
+        species: [
+          {
+            name: "Requin-taupe commun",
+            status: "status ?",
+            year: "Année ?",
+          },
+        ],
+      }),
+    );
+  });
 
-function extractDataFromAssessmentAPICall(userLanguage, response) {
-  console.info(`Processing ${response.data.taxon.scientific_name}`);
-  let returned = {
-    id: response.data.assessmentId,
-    latin_name: response.data.taxon.scientific_name,
-    type: "",
-    common_names: response.data.taxon.common_names.reduce(function (map, obj) {
-      map[obj.language] = obj.name;
-      return map;
-    }, {}),
-    status: response.data.red_list_category.description.en,
-    year: response.data.year_published,
-  };
-  console.info(`Processed ${response.data.taxon.scientific_name}`);
-  return returned;
+  function extractDataFromAssessmentAPICall(userLanguage, response) {
+    console.info(`Processing ${response.data.taxon.scientific_name}`);
+    let returned = {
+      id: response.data.assessmentId,
+      latin_name: response.data.taxon.scientific_name,
+      type: "",
+      common_names: response.data.taxon.common_names.reduce(function (
+        map,
+        obj,
+      ) {
+        map[obj.language] = obj.name;
+        return map;
+      }, {}),
+      status: response.data.red_list_category.description.en,
+      year: response.data.year_published,
+    };
+    console.info(`Processed ${response.data.taxon.scientific_name}`);
+    return returned;
+  }
+
+  app.get("/get-species-for/:codePays", async function (req, res) {
+    console.info(
+      `Searching for endangered species in "${req.params.codePays}"`,
+    );
+    // This is dangerous !
+    const url = `https://api.iucnredlist.org/api/v4/countries/${req.params.codePays}`;
+    return axios
+      .get(url, IUCN_AUTH_HEADER)
+      .then(function (response) {
+        let assessments = response.data.assessments;
+        //      assessments = assessments.slice(0, Math.min(assessments.length, 10));
+        console.info(`Processing ${assessments.length} endangered species`);
+        // Hack that to check some things
+        Promise.all(
+          assessments.map(function (assessment) {
+            const assessmentId = assessment.assessment_id;
+            const url = `https://api.iucnredlist.org/api/v4/assessment/${assessmentId}`;
+            return axios.get(url, IUCN_AUTH_HEADER);
+          }),
+        ).then(function (responseArray) {
+          let transformedAssessments = responseArray.map(function (response) {
+            return extractDataFromAssessmentAPICall(req.locale, response);
+          });
+          console.info(`Processed ${assessments.length} endangered species`);
+          res.send(transformedAssessments);
+        });
+      })
+      .catch(function (error) {
+        console.error(
+          `Fetching endangered species in ${req.params.codePays} returned ${error.message}`,
+        );
+        res.status(500).send(error.message);
+      });
+  });
+
+  // Démarrage du serveur
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Serveur démarré à http://localhost:${port}`);
+  });
 }
 
-app.get("/get-species-for/:codePays", async function (req, res) {
-  console.info(`Searching for endangered species in "${req.params.codePays}"`);
-  // This is dangerous !
-  const url = `https://api.iucnredlist.org/api/v4/countries/${req.params.codePays}`;
-  return axios
-    .get(url, IUCN_AUTH_HEADER)
-    .then(function (response) {
-      let assessments = response.data.assessments;
-      //      assessments = assessments.slice(0, Math.min(assessments.length, 10));
-      console.info(`Processing ${assessments.length} endangered species`);
-      // Hack that to check some things
-      Promise.all(
-        assessments.map(function (assessment) {
-          const assessmentId = assessment.assessment_id;
-          const url = `https://api.iucnredlist.org/api/v4/assessment/${assessmentId}`;
-          return axios.get(url, IUCN_AUTH_HEADER);
-        }),
-      ).then(function (responseArray) {
-        let transformedAssessments = responseArray.map(function (response) {
-          return extractDataFromAssessmentAPICall(req.locale, response);
-        });
-        console.info(`Processed ${assessments.length} endangered species`);
-        res.send(transformedAssessments);
-      });
-    })
-    .catch(function (error) {
-      console.error(
-        `Fetching endangered species in ${req.params.codePays} returned ${error.message}`,
-      );
-      res.status(500).send(error.message);
-    });
-});
-
-// Démarrage du serveur
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Serveur démarré à http://localhost:${port}`);
-});
-
-module.exports = [getCountries];
+main();
